c623e11808f77442b24e3f40de6822001524bf63
Safi-Github <tint.address@gmail.com>
2024-02-12 10:23:09 +0430

    inbox and outbox senario

--- models/egp_letter.py
+++ models/egp_letter.py
@@ -13,26 +13,26 @@ class EgpLetter(models.Model):
     content = fields.Html('Content', required=True)
     source_department_id = fields.Many2one('hr.department', string='Source Department',
                                            default=lambda self: self.env.user.department_id.id, required=True)
-    state = fields.Selection([('draft', 'Draft'), ('sent', 'Sent')], string='Status', default='draft')
-    mail = fields.Selection([('inbox', 'Inbox'), ('outbox', 'Outbox')], string='Email', default='outbox')
+    state = fields.Selection([('inbox', 'Inbox'), ('outbox', 'Outbox')], string='Receives', default='inbox')
+    mail = fields.Selection([('inbox', 'Inbox'), ('outbox', 'Outbox')], string='Sent', default='outbox')
 
     execution_ids = fields.One2many('egp.letter.execution', 'letter_id', string='Executions')
 
-    # sent_records = fields.Integer(string='Sent Records', compute='_compute_sent_records')
+
+
+
+
+
+    filtered_record = fields.Many2one('egp.letter', compute='_compute_filtered_record', string='Filtered Record', store=True)
     #
-    # def _compute_sent_records(self):
+    # @api.depends('recipients')
+    # def _compute_filtered_record(self):
     #     for record in self:
-    #         sent_records = self.env['egp.letter'].search_count([('create_uid', '=', self.env.user.id)])
-    #         record.sent_records = sent_records
+    #         filtered_records = self.env['egp.letter'].search([('recipients', '=', 4)], limit=1)
+    #         record.filtered_record = filtered_records
 
 
-    # letter_id = fields.Many2one('egp.letter', 'Letter ID',domain=[('recipients','=',[7])])
-    # department_id_test = fields.One2many(string='Letter', compute='_compute_letter_records')
-    # @api.depends('department_id_test')
-    # def _compute_letter_records(self):
-    #     for record in self:
-    #         letter_records = self.env['egp.letter'].search([('recipients', '=', 7)])
-    #         record.department_id_test = letter_records
+
 
 
 #     state_description = fields.Many2one('egp.letter', compute='_compute_state_get')
