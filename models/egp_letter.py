8ed8286eb55acfff62890921966da79902666e5e
Safi-Github <tint.address@gmail.com>
2024-02-26 11:23:56 +0430

    fixing the issue of inbox letters

--- models/egp_letter.py
+++ models/egp_letter.py
@@ -23,28 +23,21 @@ class EgpLetter(models.Model):
 
     employeeGet_id = fields.Many2one('hr.employee', compute='_compute_user_empid')
     department_id = fields.Many2one('hr.department', compute='_compute_department_id',default=2)
-    # department_id2 = fields.Integer(string='Department id 2',default=2)
+
 
     field_name = fields.Selection([('inbox', 'Inbox')], string='Receives', compute='print_mail_field', store=True)
     def _compute_user_empid(self):
             employee = self.env['hr.employee'].search([('user_id', '=', self.env.user.id)], limit=1)
             self.employeeGet_id = employee.id
             print('the employee gotted id', self.employeeGet_id)
-
     @api.depends('employeeGet_id')
     def _compute_department_id(self):
             departments = self.env['hr.department'].search([('manager_id', '=', self.employeeGet_id.id)], limit=1)
             self.department_id = departments.id
             print('department gotted id ', self.department_id)
 
-    @staticmethod
-    def get_domain():
-        # You can define your logic here to compute the domain dynamically
-        # For example, let's say you want to filter records where my_field is not empty
-        return 3
-
-
-
-
+            action = self.env.ref('egp_letter.action_egp_letter_warida_tree_view')
+            computed_value = self.department_id.id
+            action.write({'context': {'default_desired_value': computed_value}})
 
 
